a5c822d6465e061ec2fabf2816f09071ea484332
liushengkai <949919743@qq.com>
2023-11-30 17:08:51 +0800

    feat: theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ export default {
   // daisyUI config (optional - here are the default values)
   daisyui: {
     themes: ['light', 'dark', 'retro', 'synthwave'], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: 'retro', // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes

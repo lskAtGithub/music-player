518bc40cc4f2542f343a2a0c2fc579dc37173a9a
liushengkai <949919743@qq.com>
2023-11-30 18:52:15 +0800

    feat: platte icon adjust

--- src/svgIcon/platte.tsx
+++ src/svgIcon/platte.tsx
@@ -2,7 +2,7 @@ import { defineComponent } from 'vue'
 
 function platte(): JSX.Element {
   return (
-    <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="18" height="18" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M24 44C29.9601 44 26.3359 35.136 30 31C33.1264 27.4709 44 29.0856 44 24C44 12.9543 35.0457 4 24 4C12.9543 4 4 12.9543 4 24C4 35.0457 12.9543 44 24 44Z"
         fill="none"

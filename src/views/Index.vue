1debd2ea8a4a4393858e57852f9ec7d2fc8ad3b2
liushengkai <949919743@qq.com>
2023-11-30 17:09:51 +0800

    feat: console

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useChangeTheme } from '@/hooks/theme'
+
+const onChangeTheme = () => {
+  useChangeTheme('retro')
+}
+</script>
 
 <template>
   <div class="p-4">
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <button class="btn">Button</button>
+    <button class="btn" @click="onChangeTheme">Button</button>
   </div>
 </template>

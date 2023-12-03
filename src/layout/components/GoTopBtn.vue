09a3b655c6586e8550a465fd88456f778c571a97
liushengkai <949919743@qq.com>
2023-12-03 23:01:58 +0800

    feat: go top

---
+++ src/layout/components/GoTopBtn.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { GoTop } from '@/iconPark'
+
+const props = defineProps({
+  show: Boolean
+})
+const emits = defineEmits(['goTop'])
+
+function onGoTop() {
+  emits('goTop')
+}
+</script>
+
+<template>
+  <div class="go-top-box" v-show="props.show">
+    <el-button :icon="GoTop" @click="onGoTop"></el-button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.go-top-box {
+  position: fixed;
+  right: 18px;
+  bottom: 30px;
+}
+</style>

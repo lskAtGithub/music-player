94b35af7f7439ba40619a39eba1f2e6fb1d8759c
liushengkai <949919743@qq.com>
2023-12-03 23:02:57 +0800

    feat: page plain

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Index from '../views/Index.vue'
+import Layout from '@/layout/LayoutIndex.vue'
+
+import HomePage from '@/views/HomePage.vue'
+import Setting from '@/views/Setting.vue'
+import Explore from '@/views/Explore.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +11,24 @@ const router = createRouter({
     {
       path: '/',
       name: 'Index',
-      component: Index
+      component: Layout,
+      children: [
+        {
+          path: '/',
+          name: 'HomePage',
+          component: HomePage
+        },
+        {
+          path: '/setting',
+          name: 'Setting',
+          component: Setting
+        },
+        {
+          path: '/explore',
+          name: 'Explore',
+          component: Explore
+        }
+      ]
     }
   ]
 })

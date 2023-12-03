0fd9386387890c298c31a9a60eb67bd2d74af6af
liushengkai <949919743@qq.com>
2023-12-03 23:02:24 +0800

    feat: pages plain

---
+++ src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="home-box">
+    <p v-for="i in 100" :key="i">{{ i }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home-box {
+  height: 200vh;
+}
+</style>

88c20001c79e509c00a667359694ba68f496df75
liushengkai <949919743@qq.com>
2023-12-04 15:50:38 +0800

    feat: axios 请求封装

---
+++ src/utils/request.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'https://music-api-theta-one.vercel.app/',
+  timeout: 10000
+})
+
+instance.interceptors.request.use(
+  (config) => {
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+instance.interceptors.response.use(
+  (res) => {
+    return res.data
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+export default instance

c1bfd90df619a6e2af403a7e74b0e9c27b79e0b1
liushengkai <949919743@qq.com>
2023-12-05 18:32:14 +0800

    feat： music api

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,5 +1,8 @@
 import request from '@/utils/request'
 
+/**
+ * @description: 获取热门歌手
+ */
 export function getHotArtists() {
   return request({
     url: '/top/artists',
@@ -7,9 +10,52 @@ export function getHotArtists() {
   })
 }
 
+export function testApi(params: any) {
+  return request({
+    url: '/toplist',
+    method: 'get',
+    params
+  })
+}
+
+/**
+ * @description: 获取所有榜单信息
+ */
+export function getAllList() {
+  return request({
+    url: '/toplist/detail',
+    method: 'get'
+  })
+}
+
+/**
+ * @description: 获取推荐歌单
+ */
 export function getPersonalized() {
   return request({
     url: '/personalized',
     method: 'get'
+  })
+}
+
+/**
+ * @description 获取推荐新歌
+ */
+export function getPersonalizedNewSong(params: any) {
+  return request({
+    url: '/personalized/newsong',
+    method: 'get',
+    params
+  })
+}
+
+/**
+ * @description 根据歌ID获取歌曲播放地址
+ */
+export function getSongUrl(params: { id: number | string }) {
+  return request({
+    url: '/song/url',
+    method: 'get',
+    params
   })
 }

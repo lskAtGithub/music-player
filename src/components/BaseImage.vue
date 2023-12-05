93aa747a19fae12cfc76a113d4877df5c3f5e3a0
liushengkai <949919743@qq.com>
2023-12-05 18:31:50 +0800

    feat: image 组件

--- src/components/BaseImage.vue
+++ src/components/BaseImage.vue
@@ -19,10 +19,11 @@ const props = defineProps({
 <template>
   <div
     class="base-image-box"
+    :style="props.style"
     @mouseenter="() => (isShow = true)"
     @mouseleave="() => (isShow = false)"
   >
-    <el-image class="main-img" :style="props.style" :src="props.src" :fit="props.fit" />
+    <img class="main-img" :style="props.style" :src="props.src" :fit="props.fit" />
     <div class="mask" v-show="isShow">
       <Play v-if="props.iconName === 'Play'" v-bind:color="'#fff'" />
       <View v-if="props.iconName === 'View'" v-bind:color="'#fff'" />
@@ -33,11 +34,18 @@ const props = defineProps({
 <style scoped lang="scss">
 .base-image-box {
   position: relative;
+  width: 10vw;
+  height: 10vw;
+  min-width: 125px;
+  min-height: 125px;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 
   .main-img {
     width: 10vw;
-    min-width: 125px;
     height: 10vw;
+    min-width: 125px;
     min-height: 125px;
     border-radius: 5px;
   }

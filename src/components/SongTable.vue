e488bb503de0bd6b57573dbe712a932fe7e08c2c
liushengkai <949919743@qq.com>
2023-12-06 20:23:27 +0800

    feat:  song list table component

--- src/components/SongTable.vue
+++ src/components/SongTable.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
+import type { PropType } from 'vue'
+
+type FixedType = 'left' | 'right' | true
+type AlignType = 'left' | 'center' | 'right'
+type ColumnItem =
+  | ({ slotName: string } & { prop?: never })
+  | (({ prop: string } & { slotName?: never }) & {
+      width?: string | number
+      type?: string
+      index?: number
+      label: string
+      ['column-key']?: string
+      ['min-width']?: string | number
+      fixed?: FixedType
+      align?: AlignType
+      ['class-name']?: string
+    })
+
 const props = defineProps({
   list: {
     type: Array,
+    default: () => []
+  },
+  columns: {
+    type: Array as PropType<ColumnItem[]>,
     default: () => []
   }
 })
@@ -9,10 +31,16 @@ const props = defineProps({
 
 <template>
   <el-table :data="props.list" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
+    <el-table-column v-for="item in props.columns" v-bind="item">
+      <template v-slot="scope">
+        <slot v-if="item.slotName" :name="item.slotName" :scope="scope.row" />
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+* {
+  user-select: none;
+}
+</style>

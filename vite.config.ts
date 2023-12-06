44a341b949aa52d3659c2ba5a278a00c99528122
liushengkai <949919743@qq.com>
2023-12-06 20:38:20 +0800

    feat: 打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/music-player',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {

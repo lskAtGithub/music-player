a0c263e6f99a6be017be70ed8e4e857ab12b4652
liushengkai <liushengkai95@gm,ail.com>
2023-12-18 21:38:59 +0800

    feat:  vpn 提示

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+import { ElMessageBox } from 'element-plus'
+
+onMounted(() => {
+  if (!localStorage.getItem('music-player-no-prompt')) {
+    ElMessageBox.confirm('如出现无法访问请打开VPN', '提示', {
+      confirmButtonText: '我知道了',
+      cancelButtonText: '不再提示',
+      type: 'warning',
+      closeOnPressEscape: false
+    }).catch(() => {
+      localStorage.setItem('music-player-no-prompt', 'true')
+    })
+  }
+})
 </script>
 
 <template>
